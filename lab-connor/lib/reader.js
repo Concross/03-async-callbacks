17bc3f7183d4b72c817c17868fb737a1d72c07c1
Concross <cacrossley22@gmail.com>
2018-09-07 17:01:50 -0700

    reader() throws TypeError is first param is invalid

--- lab-connor/lib/reader.js
+++ lab-connor/lib/reader.js
@@ -2,6 +2,7 @@
 
 module.exports = exports = function (paths, callback) {
   _checkNumberOfArgs(arguments, exports);
+  _checkValidFirstArg(paths);
 
 };
 
@@ -9,4 +10,14 @@ const _checkNumberOfArgs = (actual, expected) => {
   if (actual.length !== expected.length) {
     throw new Error('Error: reader() not called with proper number of arguments');
   }
+};
+
+const _checkValidFirstArg = (paths) => {
+  // check if paths is an array
+  if (!(paths instanceof Array)) {
+    throw new TypeError('Type Error: reader() was called with invalid args');
+  }
+  // check if paths has enough elements
+  // check if each element in the path is a proper file path
+
 };
