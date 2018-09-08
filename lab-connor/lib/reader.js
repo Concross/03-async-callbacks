9bc80a18ab5e1886435a0229eaaedf635b88ceec
Concross <cacrossley22@gmail.com>
2018-09-08 14:58:55 -0700

    validate paths param as array, tests pass

--- lab-connor/lib/reader.js
+++ lab-connor/lib/reader.js
@@ -14,10 +14,26 @@ const _checkNumberOfArgs = (actual, expected) => {
 
 const _checkValidFirstArg = (paths) => {
   // check if paths is an array
-  if (!(paths instanceof Array)) {
-    throw new TypeError('Type Error: reader() was called with invalid args');
-  }
+  _isPathsArrayValid(paths);
   // check if paths has enough elements
   // check if each element in the path is a proper file path
 
+};
+
+const _isPathsArrayValid = (paths) => {
+  _isPathsAnArray(paths);
+  _isPathsLengthValid(paths);
+
+};
+
+const _isPathsAnArray = (paths) => {
+  if (!Array.isArray(paths)) {
+    throw new TypeError('Type Error: reader() paths parameter must be of type Array');
+  }
+};
+
+const _isPathsLengthValid = (paths) => {
+  if (!(paths.length === 3)) {
+    throw new TypeError('Type Error: reader() paths parameter must be an array of exactly three file paths');
+  }
 };
--- lab-connor/test/reader.test.js
+++ lab-connor/test/reader.test.js
@@ -17,7 +17,7 @@ describe('Testing reader(paths, callback) for valid arguments', () => {
   });
 
   test('reader() should throw a TypeError when called with improper type for first param', () => {
-    let typeErrorMessage = 'Type Error: reader() was called with invalid args';
+    let typeErrorMessage = 'Type Error: reader() paths parameter must be of type Array';
     let cb = jest.fn();
     expect(() => {
       reader('badArg', cb);
@@ -37,6 +37,19 @@ describe('Testing reader(paths, callback) for valid arguments', () => {
 
     expect(() => {
       reader(undefined, cb);
+    }).toThrowError(typeErrorMessage);
+
+  });
+
+  test('reader() should throw a TypeError when paths param is not an array of length 3', () => {
+    let typeErrorMessage = 'Type Error: reader() paths parameter must be an array of exactly three file paths';
+    let cb = jest.fn();
+    expect(() => {
+      reader([1, 2], cb);
+    }).toThrowError(typeErrorMessage);
+
+    expect(() => {
+      reader([1, 2, 3, 4], cb);
     }).toThrowError(typeErrorMessage);
 
   });
